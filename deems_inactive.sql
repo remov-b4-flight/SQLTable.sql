2083c24e3e42a7d5c2ceb6815b963dc9fb8311b5
remov-b4-flight <remove_b4_flight@icloud.com>
2022-11-21 05:57:06 +0900

    add gitignore

--- deems_inactive.sql
+++ deems_inactive.sql
@@ -2,7 +2,7 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE view [dbo].[deems_inactive] as 
-select caption,[dateTime],collectionSet from ( select *, row_number() over (PARTITION BY caption ORDER BY [dateTime] DESC) AS number FROM photos) as lastshot 
+ALTER view [dbo].[deems_inactive] as 
+select caption,[dateTime],collectionSet,collectionName from ( select *, row_number() over (PARTITION BY caption ORDER BY [dateTime] DESC) AS number FROM photos) as lastshot 
 where lastshot.number = 1 and lastshot.[dateTime] between DATEADD(year, -5, GETDATE()) and DATEADD(year, -1, GETDATE()) ;
 GO
--- deems_retire.sql
+++ deems_retire.sql
@@ -2,8 +2,8 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE view [dbo].[deems_retire] as 
-select caption,[dateTime],collectionSet from ( select *, row_number() over (PARTITION BY caption ORDER BY [dateTime] DESC) AS number FROM photos) as lastshot 
+ALTER view [dbo].[deems_retire] as 
+select caption,[dateTime],collectionSet,collectionName from ( select *, row_number() over (PARTITION BY caption ORDER BY [dateTime] DESC) AS number FROM photos) as lastshot 
 where lastshot.number = 1 and lastshot.[dateTime] < DATEADD(year, -5, GETDATE()) 
 and collectionSet != '%RETIRED'
 GO
