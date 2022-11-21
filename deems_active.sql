16ac70567ae58a7ebbbe3dd37dfc32c5d3554097
remov-b4-flight <remove_b4_flight@icloud.com>
2022-11-21 22:35:20 +0900

    modify deems_active.sql

--- deems_active.sql
+++ deems_active.sql
@@ -2,7 +2,7 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-create view [dbo].[deems_active] as 
+ALTER view [dbo].[deems_active] as 
 select caption,[dateTime],collectionSet from ( select *, row_number() over (PARTITION BY caption ORDER BY [dateTime] DESC) AS number FROM photos) as lastshot 
 where lastshot.number = 1 
 and lastshot.[dateTime] > DATEFROMPARTS(YEAR(DATEADD(year, -1, GETDATE())),1,1) 
