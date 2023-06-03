4fd46d096480a23ad0dd90fceb7955fc75324a60
remov-b4-flight <remove_b4_flight@icloud.com>
2023-06-04 07:39:23 +0900

    fix deems_hof script

--- deems_hof.sql
+++ deems_hof.sql
@@ -4,6 +4,5 @@ SET QUOTED_IDENTIFIER ON
 GO
 create view [dbo].[deems_hof] as SELECT [caption]
       ,[counter]
-  FROM (select caption,count(*) as counter from photos group by caption) 
-where counter > 2000 
+  FROM (select caption,count(*) as counter from photos group by caption) as s where s.counter > 2000 
 GO
